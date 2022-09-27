77336b1acd716bd5d3b58af3051dd07b9a948187
brewerj3 <brewerj3@hawaii.edu.com>
2022-09-27 10:30:21 -1000

    Initial commit

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+char msg1[6] = "civic";
+char msg2[7] = "wiliki";
+char msg3[40];
+char msg_yes[7] = ": yes\n";
+char msg_no[6] = ": no\n";
+
+void display_result(char msg[], int result)
+{
+    printf("%s",msg);
+    if (result==1) {
+        printf("%s",msg_yes);
+    }
+    else {
+        printf("%s",msg_no);
+    }
+}
+
+int msg_length(char msg[])
+{
+    int length=0;
+    while (msg[length] != '\0') {
+        length++;
+    }
+    return length;
+}
+
+int palindrome(char msg[], int length)
+{
+    return 0;
+}
+
+void main()
+{
+    int length = msg_length(msg1);
+    int result = palindrome(msg1, length);
+    display_result(msg1, result);
+
+    length = msg_length(msg2);
+    result = palindrome(msg2, length);
+    display_result(msg2, result);
+
+    exit(0);
+}
+
+
+
+
